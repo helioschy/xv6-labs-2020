abe18f71930f6ee15f1508e61ba8ca6a487fdb09
helioschy <810863859@qq.com>
2023-10-19 17:18:23 +0800

    utli

--- user/user.h
+++ user/user.h
@@ -7,7 +7,7 @@ int exit(int) __attribute__((noreturn));
 int wait(int*);
 int pipe(int*);
 int write(int, const void*, int);
-int read(int, void*, int);
+int read(int, void*, int); 
 int close(int);
 int kill(int);
 int exec(char*, char**);
